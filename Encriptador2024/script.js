c0fe156806efae9055e86042f20443331ddca7a7
Shelvy Carrasco Ore <scarrascoore@gmail.com>
2024-03-12 18:34:27 -0500

    Pagina realizada de un encriptador de Alura ONE - Oracle Next Education

---
+++ Encriptador2024/script.js
@@ -0,0 +1,70 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value);
+  const informacion = document.querySelector('.informacion');
+  const imagenInfo = document.querySelector('.imagen-info');
+  const btnCopiar = document.querySelector('.btn-copiar');
+  mensaje.value = textoEncriptado;
+  textArea.value ="";
+  mensaje.style.backgroundImage = "none";
+  imagenInfo.style.display = "none";
+  btnCopiar.style.display = 'block';  
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
+    }
+  }
+  return stringEncriptada;
+}
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value ="";
+    mensaje.style.backgroundImage = "none";
+  }
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [
+      ["e", "enter"],
+      ["i", "imes"],
+      ["a", "ai"],
+      ["o", "ober"],
+      ["u", "ufat"],
+    ];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+  
+    for (let i = 0; i < matrizCodigo.length; i++) {
+      if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+        stringDesencriptada = stringDesencriptada.replaceAll(
+          matrizCodigo[i][1],
+          matrizCodigo[i][0]
+        );
+      }
+    }
+    return stringDesencriptada;
+
+  }
+
+
+  function copiarMensaje() {    
+    textArea.value = mensaje.value;
+}
